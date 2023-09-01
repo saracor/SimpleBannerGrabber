ff61b87c1eab7c00954238991426b7169b586a86
Sara Korpila <sara.korpila@gmail.com>
2023-09-01 17:01:13 +0300

    Added SSL support for https

--- simplebannergrabber.py
+++ simplebannergrabber.py
@@ -1,6 +1,7 @@
 #! /usr/bin/python3
 
 import socket
+import ssl
 
 print(
 	'''
@@ -30,6 +31,27 @@ def grabit(target, port):
 			print("TimeoutError: timed out")
 		except ConnectionRefusedError:
 			print("ConnectionRefusedError: [Errno 111] Connection refused")
+	
+	elif port == 443:
+		try:
+			host = input("Give the server hostname (i.e google.com) > ")
+			print()
+			Purpose = ssl.Purpose.SERVER_AUTH
+			context = ssl.create_default_context(purpose=Purpose)
+			with socket.create_connection((target,port)) as sock:
+				with context.wrap_socket(sock, server_hostname=host) as ssock:
+					ssock.settimeout(2)
+					ssock.send("HEAD / HTTP/1.1\r\nUser-Agent: Mozilla/5.0\r\nHost: 1.2.3.4\r\nAccept: */*\r\n\r\n".encode())
+					data = ssock.recv(1024).decode()
+					print(data)
+					ssock.close()
+		except ssl.SSLCertVerificationError:
+			print("ssl.SSLCertVerificationError: possible hostname mismatch")
+		except TimeoutError:
+			print("TimeoutError: timed out")
+		except ConnectionRefusedError:
+			print("ConnectionRefusedError: [Errno 111] Connection refused")
+	
 	else:
 		try:
 			s.connect((target, port))
@@ -46,7 +68,7 @@ while True:
 	targetPORT = int(input("Target Port > "))
 	grabit(targetIP, targetPORT)
 	print()
-	newport = input("Check another port? y/n > ")
+	newport = input("Check another port for same target? y/n > ")
 	print()
 	newport = newport.lower()
 	if newport == "y":
